ea1c1ea0c3ed1856f2ad33e588674267ed8253b5
shahzeb <shahzeb3939@gmail.com>
2020-11-30 14:05:52 +0500

    Initial commit.

---
+++ serenitydemo/src/main/java/net/serenity/demo/app/Calculator.java
@@ -0,0 +1,7 @@
+package net.serenity.demo.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ serenitydemo/src/test/java/net/serenity/demo/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package net.serenity.demo.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="net.serenity.demo")
+public class AcceptanceTestSuite {}
